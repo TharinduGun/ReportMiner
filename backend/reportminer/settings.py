2a24b7aa73410318658c131386c491f9df1fa1d9
TharinduGun <gunathunga18@gmail.com>
2025-05-06 18:04:52 +0530

    postgresql db activation

--- backend/reportminer/settings.py
+++ backend/reportminer/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'reportminer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reportminer',
+        'USER': 'tharindu',
+        'PASSWORD': 'tharindu123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
